42bbb10082e1c8b732c63c1d31b9286434b24659
pinkeshroy <pinkeshyadav9661@gmail.com>
2025-04-20 11:52:38 +0530

    add cors origin

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import jobRoutes from './routes/jobRoutes.js';
-import { authenticate } from './middleware/authenticate.js';
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import jobRoutes from "./routes/jobRoutes.js";
+import { authenticate } from "./middleware/authenticate.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://job-tracker-one-bay.vercel.app", "http://localhost:5001"],
+  })
+);
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/job', authenticate, jobRoutes);
-
-
-
+app.use("/api/auth", authRoutes);
+app.use("/api/job", authenticate, jobRoutes);
 
 export default app;
